51fa961d552cd9567bd4d8f03f6e3c5d4a402f13
kana <yuiwiz928_sub@yahoo.co.jp>
2023-03-02 21:50:19 +0900

    First Commit

---
+++ src/main/java/me/hotal/paper/autowhitelistadder/AutoWhitelist.java
@@ -0,0 +1,30 @@
+package me.hotal.paper.autowhitelistadder;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AutoWhitelist extends JavaPlugin implements Listener {
+
+    public void onEnable() {
+        // Pluginが有効になったときに呼び出される処理
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    public void onDisable() {
+        // Pluginが無効になったときに呼び出される処理
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (!player.isWhitelisted()) {
+            player.setWhitelisted(true);
+            Bukkit.broadcastMessage(ChatColor.GREEN + player.getName() + "がホワイトリストに追加されました。");
+        }
+    }
+}
